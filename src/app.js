aa302dd02f92f48135c1b158340a36429722bb34
rubsuadav <rsuarezdavid@gmail.com>
2023-05-27 14:55:01 +0200

    now we can retrieved all the posibles endpoints

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import employeersRoutes from "./routes/employeers.routes.js";
+import listEndpoints from "express-list-endpoints";
 import "./config.js";
 
 const app = express();
@@ -9,6 +10,18 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/api", employeersRoutes);
+
+//MUESTRA TODAS LAS RUTAS DISPONIBLES
+app.get("/", (req, res) => {
+  const routes = listEndpoints(app).map((route) => {
+    return {
+      path: route.path,
+      methods: route.methods,
+      };
+  });
+  res.json({ routes });
+});
+
 app.use((req, res) => {
   res.status(404).json({ msg: "Not found" });
 });
